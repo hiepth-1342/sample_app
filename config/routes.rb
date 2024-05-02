4caa08bf387b3dcf504bd955d3e8ca033ab8f0c9
hiepth-1342 <trinh.huu.hiep@sun-asterisk.com>
2024-05-02 15:48:39 +0700

    Chapter 3,4,5

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  scope "(:locale)", locale: /en|vi/ do
+    get "help", to: "pages#help"
+    get "pages/users"
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+    # Defines the root path route ("/")
+    root "pages#home"
+  end
 end
