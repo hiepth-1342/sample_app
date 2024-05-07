0c5a0613613784ef6683a0b139f3e022aabfc2a4
hiepth-1342 <trinh.huu.hiep@sun-asterisk.com>
2024-05-07 16:16:20 +0700

    chapter 8: Basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
+
     get "help", to: "pages#help"
     get "/signup", to: "users#new"
     post "/signup", to: "users#create"
