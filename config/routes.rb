f71f55ca3af730d9837eb69b3ecdd16727cd717c
hiepth-1342 <trinh.huu.hiep@sun-asterisk.com>
2024-05-07 09:29:14 +0700

    Chapter 7: Sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     get "help", to: "pages#help"
-    get "pages/users"
+    get "/signup", to: "users#new"
+    post "/signup", to: "users#create"
+    resources :users, only: %i(new, create, show)
     # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
     # Defines the root path route ("/")
